6656b4b24186db8f3102d0f1cfd3ea86b795ed39
NGC3031 <scott.d@ieee.org>
2019-03-24 20:30:58 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#JS-basics
+Algorithm examples in JavaScrip that I have used for teaching and self learning 
+over the years. Use at your own risk. Do not use algorithms if you dont get how
+it works or what it is doing. These are just demo algorithms so there might be 
+better algorithms for what you are wanting to do.
+
+-Live long and prosper.
---
+++ connectivityQuickSort.js
@@ -0,0 +1,48 @@
+// Connectivity Quick sort. Use at own risk for path finding
+// the outputs are left in if you want to see the step by step updates
+// Just uncomment them.
+// Finds the connections from pairs of numbers in the numberPairs array and 
+// displays the connections 
+const N = 20;
+var numberPairs = [];
+var storeArray = [];
+var str = ' ';
+//create array of numbers for testing
+for (var i = 0; i < N; i++) {
+    numberPairs[i] = Math.floor(Math.random() * N);
+}
+//console.log(numberPairs.toString());
+
+//create storage array
+for (var i = 0; i < N; i++) {
+    storeArray[i] = 0;
+}
+//console.log(storeArray.toString());
+//console.log(numberPairs.length);
+
+// if p!=q then enter the 2nd value in the pth cell of storeArray
+for (var i = 0; i < N - 1; i++) {
+    var p = numberPairs[i];
+    var q = numberPairs[i + 1];
+    if (p != q) {
+        storeArray[p] = q;
+    }
+    console.log(storeArray.toString());
+}
+console.log(numberPairs.toString());
+
+console.log('Connections:');
+//look for equal values in array cells and output the paths
+for (var i = 0; i < N; i++) {
+    //get the first value and find where else it occurs
+    p = storeArray[i];
+    str = p;
+    for (var j = 1; j < N; j++) {
+        if (j != i) {
+            if (p == storeArray[j]) {
+                str = str + '-' + j;
+            }
+        }
+    }
+    console.log(str);
+}
